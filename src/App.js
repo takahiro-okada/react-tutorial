fc3546114e320c7f34dd9d56a3ecfc88b83ae331
takahiro-okada <okahanako09875@gmail.com>
2023-08-23 05:43:57 +0900

    update: Adding usestate

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from 'react';
 import Square from './Square';
 
 export default function Board() {
+
   return (
     <div>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </div>
   );
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function Square({value}){
+export default function Square(){
+
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
   return(
-    <button className="square">{value}</button>
+    <button className="square" onClick={handleClick}>{value}</button>
   )
 }
